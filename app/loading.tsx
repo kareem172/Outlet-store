c76b27214bb6f3150c48baf27279d202ff071232
kareem172 <kareemkhalaf1722@gmail.com>
2025-05-18 16:09:50 +0300

    feat: simplify loading component with spinner animation; remove unnecessary elements

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,18 +2,8 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
-        Loading...
-      </h1>
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(min(250px,100%),1fr))] gap-6 my-10">
-        {Array.from({ length: 10 }, (_, index) => (
-          <div
-            key={index}
-            className="animate-pulse bg-gray-200 h-64 rounded-lg"
-          ></div>
-        ))}
-      </div>
+    <div className="flex items-center justify-center h-screen">
+      <span className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></span>
     </div>
   );
 }
